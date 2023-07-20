8429ad72a90bfcb9c62825b0b79374d8850f95b5
Akash <ashakyawwe06@gmail.com>
2023-07-20 23:14:57 +0530

    galat kiya h

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -19,7 +19,7 @@ const Cart = (props) => {
     cartCtx.addItem({ ...item, amount: 1 });
   };
   // kjkfsekfjk
-
+  // /galatbkita hai
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
